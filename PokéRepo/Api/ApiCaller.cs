6294a8ceef8aaca3a6adb9a3a6aeb70b99f58313
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-02-07 11:57:29 +0100

    ApiCaller to api endpoint

---
+++ PokéRepo/Api/ApiCaller.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using PokéRepo.Models;
+
+namespace PokéRepo.Api
+{
+    public class ApiCaller
+    {
+        public HttpClient Client { get; set; }
+
+        public ApiCaller()
+        {
+            Client = new();
+
+            Client.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon/");
+        }
+
+        public async Task<Root> MakeCall(string url)
+        {
+            HttpResponseMessage response = await Client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                //hämta json, 
+                string json = await response.Content.ReadAsStringAsync();
+
+                //gör om json till objekt genom api modellen för root
+                Root? result = JsonConvert.DeserializeObject<Root>(json);
+                //returnera result
+
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+
+            throw new HttpRequestException();
+        }
+
+    }
+}
---
+++ PokéRepo/Models/Apimodels.cs
@@ -0,0 +1,26 @@
+﻿namespace PokéRepo.Models
+{
+    public class Root
+    {
+        public int Id { get; set; }
+        public int Height { get; set; }
+        public int Base_experience { get; set; }
+        public string Name { get; set; }
+        public List<Ability> Abilities { get; set; }
+        public List<Form> Forms { get; set; }
+
+    }
+
+    public class Ability
+    {
+        public string Name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Form
+    {
+        public string Name { get; set; }
+        public string url { get; set; }
+    }
+
+}
