a6cfd0083d9720739fff0e32718bb1c0c4743dd9
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-02-07 15:34:42 +0100

    modified apimodel

--- PokéRepo/Models/Apimodels.cs
+++ PokéRepo/Models/Apimodels.cs
@@ -13,7 +13,14 @@
 
     public class Ability
     {
-        public string Name { get; set; }
+        public Ability2 ability { get; set; }
+        public bool is_hidden { get; set; }
+        public int slot { get; set; }
+    }
+
+    public class Ability2
+    {
+        public string name { get; set; }
         public string url { get; set; }
     }
 
